3eead620a18a2fac0151cf9e800d86ef1e9dfb99
Vojtěch Staněk <vojteskas99@gmail.com>
2023-12-29 14:12:08 +0100

    Wav2Vec

---
+++ w2v_model.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import numpy as np
+
+import fairseq # type: ignore
+
+class SSLModel(nn.Module):
+    def __init__(self,device):
+        super(SSLModel, self).__init__()
+        
+        cp_path = './xlsr2_300m.pt'
+        model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([cp_path])
+        self.model = model[0]
+        self.device=device
+        return
+
+    def extract_feat(self, input_data):
+        # put the model to GPU if it is not there
+        if next(self.model.parameters()).device != input_data.device \
+           or next(self.model.parameters()).dtype != input_data.dtype:
+            self.model.to(input_data.device, dtype=input_data.dtype)
+            self.model.train()
+
+        if True:
+            # input should be in shape (batch, length)
+            if input_data.ndim == 3:
+                input_tmp = input_data[:, :, 0]
+            else:
+                input_tmp = input_data
+                
+            # [batch, length, dim]
+            emb = self.model(input_tmp, mask=False, features_only=True)['x']
+        return emb
