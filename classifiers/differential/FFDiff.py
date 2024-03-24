9b0528d232b73ed02e97cefe35b3ce5b16884d7d
Vojtěch Staněk <vojteskas99@gmail.com>
2024-03-24 14:09:22 +0100

    Remove "2" variants from FFDiff
    Reason is that it would require equal lenght recordings for it to work

--- classifiers/differential/FFDiff.py
+++ classifiers/differential/FFDiff.py
@@ -89,49 +89,6 @@ class FFDiff(FFDiffBase):
         return out, prob
 
 
-class FFDiff2(FFDiffBase):
-    """
-    Linear classifier using subtraction as a difference metric.
-    """
-
-    def __init__(self, extractor, feature_processor, in_dim=1024):
-        """
-        Initialize the model.
-
-        param extractor: Model to extract features from audio data.
-                         Needs to provide method extract_features(input_data)
-        param feature_processor: Model to process the extracted features.
-                                 Needs to provide method __call__(input_data)
-        param in_dim: Dimension of the input data to the classifier, divisible by 4.
-        """
-
-        super().__init__(extractor, feature_processor, in_dim)
-
-    def forward(self, input_data_ground_truth, input_data_tested):
-        """
-        Forward pass through the model.
-
-        Extract features from the audio data, process them and pass them through the classifier.
-
-        param input_data_ground_truth: Audio data of the ground truth of shape: (batch_size, seq_len)
-        param input_data_tested: Audio data of the tested data of shape: (batch_size, seq_len)
-
-        return: Output of the model (logits) and the class probabilities (softmax output of the logits).
-        """
-
-        emb_gt = self.extractor.extract_features(input_data_ground_truth)
-        emb_test = self.extractor.extract_features(input_data_tested)
-
-        diff = emb_gt - emb_test
-
-        emb = self.feature_processor(diff)
-
-        out = self.classifier(emb)
-        prob = F.softmax(out, dim=1)
-
-        return out, prob
-    
-
 class FFDiffAbs(FFDiffBase):
     """
     Linear classifier using the absolute value of subtraction as a difference metric.
@@ -176,49 +133,6 @@ class FFDiffAbs(FFDiffBase):
         return out, prob
 
 
-class FFDiffAbs2(FFDiffBase):
-    """
-    Linear classifier using subtraction as a difference metric.
-    """
-
-    def __init__(self, extractor, feature_processor, in_dim=1024):
-        """
-        Initialize the model.
-
-        param extractor: Model to extract features from audio data.
-                         Needs to provide method extract_features(input_data)
-        param feature_processor: Model to process the extracted features.
-                                 Needs to provide method __call__(input_data)
-        param in_dim: Dimension of the input data to the classifier, divisible by 4.
-        """
-
-        super().__init__(extractor, feature_processor, in_dim)
-
-    def forward(self, input_data_ground_truth, input_data_tested):
-        """
-        Forward pass through the model.
-
-        Extract features from the audio data, process them and pass them through the classifier.
-
-        param input_data_ground_truth: Audio data of the ground truth of shape: (batch_size, seq_len)
-        param input_data_tested: Audio data of the tested data of shape: (batch_size, seq_len)
-
-        return: Output of the model (logits) and the class probabilities (softmax output of the logits).
-        """
-
-        emb_gt = self.extractor.extract_features(input_data_ground_truth)
-        emb_test = self.extractor.extract_features(input_data_tested)
-
-        diff = torch.abs(emb_gt - emb_test)
-
-        emb = self.feature_processor(diff)
-
-        out = self.classifier(emb)
-        prob = F.softmax(out, dim=1)
-
-        return out, prob
-
-
 class FFDiffQuadratic(FFDiffBase):
     """
     Linear classifier using the absolute value of subtraction as a difference metric.
@@ -258,49 +172,6 @@ class FFDiffQuadratic(FFDiffBase):
         diff = torch.pow(emb_gt - emb_test, 2)
 
         out = self.classifier(diff)
-        prob = F.softmax(out, dim=1)
-
-        return out, prob
-
-
-class FFDiffQuadratic2(FFDiffBase):
-    """
-    Linear classifier using subtraction as a difference metric.
-    """
-
-    def __init__(self, extractor, feature_processor, in_dim=1024):
-        """
-        Initialize the model.
-
-        param extractor: Model to extract features from audio data.
-                         Needs to provide method extract_features(input_data)
-        param feature_processor: Model to process the extracted features.
-                                 Needs to provide method __call__(input_data)
-        param in_dim: Dimension of the input data to the classifier, divisible by 4.
-        """
-
-        super().__init__(extractor, feature_processor, in_dim)
-
-    def forward(self, input_data_ground_truth, input_data_tested):
-        """
-        Forward pass through the model.
-
-        Extract features from the audio data, process them and pass them through the classifier.
-
-        param input_data_ground_truth: Audio data of the ground truth of shape: (batch_size, seq_len)
-        param input_data_tested: Audio data of the tested data of shape: (batch_size, seq_len)
-
-        return: Output of the model (logits) and the class probabilities (softmax output of the logits).
-        """
-
-        emb_gt = self.extractor.extract_features(input_data_ground_truth)
-        emb_test = self.extractor.extract_features(input_data_tested)
-
-        diff = torch.pow(emb_gt - emb_test, 2)
-
-        emb = self.feature_processor(diff)
-
-        out = self.classifier(emb)
         prob = F.softmax(out, dim=1)
 
         return out, prob
