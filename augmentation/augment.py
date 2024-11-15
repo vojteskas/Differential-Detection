9f14f6ff29ee64287fe52d8bd0addfa61c58df09
vojteskas <vojteskas99@gmail.com>
2024-11-15 17:39:17 +0100

    prepare for the augmentation pipeline

--- augmentation/augment.py
+++ augmentation/augment.py
@@ -1,13 +1,13 @@
 import torch
+import torch_audiomentations as AA # This is what we use for noise and filter augmentations
 
-from augmentation.general import change_speed
+from augmentation.general import GeneralAugmentations
+from augmentation.codec import CodecAugmentations
+from augmentation.RIR import RIRAugmentations
+from augmentation.RawBoost import process_Rawboost_feature
 
 def augment_waveform(waveform: torch.Tensor) -> torch.Tensor:
     """
     Function to define the waveform augmentation pipeline.
     """
-    # Change the speed of the audio
-    speed_factor = 1.1
-    waveform = change_speed(waveform, speed_factor)
-
-    return waveform
+    pass
