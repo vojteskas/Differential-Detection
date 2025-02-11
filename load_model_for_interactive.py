51ab19d65a83b14194686df6bb6062381dcfd698
vojteskas <vojteskas99@gmail.com>
2025-02-11 15:05:09 +0100

    Fix loading of the model, put it in eval mode by default

--- load_model_for_interactive.py
+++ load_model_for_interactive.py
@@ -5,25 +5,32 @@ import torch
 from classifiers.FFBase import FFBase
 from common import build_model
 
+from torchaudio import load
+
 
 def load_model_for_interactive():
     args: Namespace = Namespace()
     args.extractor = "XLSR_300M"
-    args.classifier = "FF"
+    args.classifier = "FFConcat3"
     
     args.processor = "MHFA"
     model_mhfa, _ = build_model(args)
     assert isinstance(model_mhfa, FFBase)
-    model_mhfa.load_state_dict(torch.load("FF_MHFA.pt", map_location=torch.device('cpu'), weights_only=True))
+    model_mhfa.load_state_dict(torch.load("FConcat3_MHFA_finetune_7.pt", map_location=torch.device('cpu'), weights_only=True))
 
-    args.processor = "AASIST"
-    model_aasist, _ = build_model(args)
-    assert isinstance(model_aasist, FFBase)
-    model_aasist.load_state_dict(torch.load("FF_AASIST_finetune_5.pt", map_location=torch.device('cpu'), weights_only=True))
+    # args.processor = "AASIST"
+    # model_aasist, _ = build_model(args)
+    # assert isinstance(model_aasist, FFBase)
+    # model_aasist.load_state_dict(torch.load("FF_AASIST_finetune_5.pt", map_location=torch.device('cpu'), weights_only=True))
 
     print("Models loaded successfully")
     # print(model_mhfa, model_aasist)
-    return model_mhfa, model_aasist
+    return model_mhfa.eval() #, model_aasist.eval()
 
 if __name__ == "__main__":
-    load_model_for_interactive()
+    f_wf, sr1 = load("fake.flac")
+    r_wf, sr2 = load("real.flac")
+
+    model = load_model_for_interactive()
+
+    print(model(r_wf, f_wf))
